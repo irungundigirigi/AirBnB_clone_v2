3041042dd452614e273aeb1aac61b82311717b05
irungundigirigi <irungundigirigi@gmail.com>
2023-11-20 23:29:19 +0300

    update tasks

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,10 @@
--- Create database hbnb_dev_db for the project
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Create a new user `hbnb_dev`in localhost  with all database privileges
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- grant all privileges to user `hbnb_dev`
-GRANT ALL PRIVILEGES ON hbnb_dev_db. * TO 'hbnb_dev'@'localhost';
+-- Create database hbnb_test_db for the project
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Create a new user `hbnb_test`in localhost  with all database privileges
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- grant all privileges to user `hbnb_test`
+GRANT ALL PRIVILEGES ON hbnb_test_db. * TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
--- Grant the SELECT privilege for the user hbnb_dev in the db performance schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Grant the SELECT privilege for the user hbnb_test in the db performance schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
