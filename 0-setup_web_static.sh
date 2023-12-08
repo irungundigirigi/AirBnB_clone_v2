daa896c749680eb70cc65480a6834780784e1494
irungundigirigi <irungundigirigi@gmail.com>
2023-12-08 05:10:01 +0300

    fix bug

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,6 +2,7 @@
 # script sets up webservers for deployment of Airbnb_clone's web static
 sudo apt-get update
 sudo apt-get -y install nginx
+suo ufw allow 'Nginx HTTP'
 
 sudo mkdir -p /data
 sudo mkdir -p /data/web_static/
@@ -11,17 +12,17 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 
 sudo echo "<html>
-<head><head>
-<body>Nginx test</body>
+    <head></head>
+    <body>Nginx test</body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i "/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}" /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-sudo systemctl nginx restart
+sudo service nginx restart
 
 
 
