b28637a5bfda8283310aac8963cc7444c3163851
irungundigirigi <irungundigirigi@gmail.com>
2023-12-10 10:46:32 +0300

    add conditional append

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
-# script sets up webservers for deployment of Airbnb_clone's web static
+
+# Update and install nginx
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
+# Create directory structure and test HTML file
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 
-sudo echo "<html>
+echo "<html>
     <head></head>
     <body>Nginx test</body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
+# Change ownership
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Check if location block exists before appending
+if ! sudo grep -q 'location /hbnb_static {' /etc/nginx/sites-enabled/default; then
+    sudo sed -i '/listen 80 default_server/ a\location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+fi
 
+# Restart nginx
 sudo service nginx restart
-
-
-
 
