d67c9b30cc9e85f86f77b39b4ad90f070d4d76e8
irungundigirigi <irungundigirigi@gmail.com>
2023-12-10 10:52:04 +0300

    update code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,13 +5,18 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-# Create directory structure and test HTML file
+# Create directory structure and test HTML filie
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 
 echo "<html>
     <head></head>
-    <body>Nginx test</body>
+    <body>Holberton School</body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
