cb4486fd8839433dfaad3e08d210205bb518a404
irungundigirigi <irungundigirigi@gmail.com>
2023-12-27 11:12:44 +0300

    add web app documentation

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """ return 'Hello HBNB!' at route / """
     return "Hello HBNB!"
 
 @app.route('/hbnb')
 def hbnb():
+    """ return "HBNB" at route /hbnb """
     return "HBNB"
 
 
