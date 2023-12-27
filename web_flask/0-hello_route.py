80fcb33ff272eb5dc4291655a9c0fa2377a9ada1
irungundigirigi <irungundigirigi@gmail.com>
2023-12-27 10:39:51 +0300

    add web app documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+    This module starts a Flask Application
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Displays 'Hello HBNB' on root URL """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
